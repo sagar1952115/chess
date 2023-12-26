0cf5bc02ca0fee6d16bd73a7fc8452439b95f466
Sagar Kumar <sagarkumar1952115@gmail.com>
2023-12-26 18:41:29 +0530

    deployed

--- chess-web/src/helpers/apiHelpers.js
+++ chess-web/src/helpers/apiHelpers.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const url = "https://chess-backend-115.onrender.com/";
+export const url = "https://chess-backend-115.onrender.com";
 // export const url = "http://localhost:3001";
 
 export const socket = io.connect(url);
